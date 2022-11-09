6a75c39ead0a27f7fc170ed720a77d9629c3038c
valentinCord <valentin.corduant@gmail.com>
2022-11-09 11:27:23 +0100

    add functions

--- A.cpp
+++ A.cpp
@@ -5,6 +5,72 @@
 #include <algorithm>
 using namespace std;
 
+void printTab(int *tab);
+void printX(int n, int X[8][2]);
+void problem_init(string input_file, int objectif_number, int costs[32][8], int opt_sol[8][2]);
+
+int main()
+{
+    // Problem parameters
+    const int problem_size = 8;
+    const int objectif_number = 2;
+    int costs[32][8];
+    int opt_sol[8][2];
+    string input_file = "input/LAP_8x8_2_obj_SOL.txt";
+
+    // Problem init to get cost matrcies and optimal solutions
+    problem_init(input_file, objectif_number, costs, opt_sol);
+
+    // Pareto Local Serach Init
+    vector<int> A; // Archive de solutions
+    vector<int> P;
+    vector<int> P_a;
+
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    // Generation d'une solution admissible aléatoire
+    int shuffle_1[problem_size];
+    int shuffle_2[problem_size];
+    for (int i = 0; i < problem_size; i++) 
+    {
+        shuffle_1[i] = i;
+        shuffle_2[i] = i;
+    }
+    random_shuffle(shuffle_1, shuffle_1 + problem_size);
+    random_shuffle(shuffle_2, shuffle_2 + problem_size);
+    int x[problem_size][2];
+    for (int i = 0; i < problem_size; i++)
+    {
+        x[i][1] = shuffle_1[i];
+        x[i][2] = shuffle_2[i];
+    }
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    // ----------------------------------------------
+    // Pareto Local Search
+    P = A;
+    P_a.clear();
+    while (!P.empty())
+    {
+        for(int i = 0; i < P.size(); i++)
+        {
+            // Recherche du voisinage de la solution P[i]
+            // Bouble sur tout le voisinage
+            // Si non x' non dominé par x
+            // Et si l'objectif de x' différent de l'objectif de x
+            // Update(A, x')
+        }
+    };
+    // ----------------------------------------------
+
+    return 1;
+}
+
+// sol -> [(i,j), ()]
+// func evaluer entrer tab[i][j] et boucler sur tous les objectifs
+// recherche locale inverser les i ou les j
+// Algo ->
+// dominance si la solution est dominée ou pas dans l'archive
+
 void printTab(int *tab)
 {
     cout << "printing " << endl;
@@ -23,72 +89,35 @@ void printX(int n, int X[8][2])
     }
 }
 
-int main()
+void problem_init(string input_file, int objectif_number, int costs[32][8], int opt_sol[8][2])
 {
-    int n, nbObj;
-    string line;
+    int problem_size;
+    ifstream inFile(input_file);
+    inFile >> problem_size;
 
-    cout << "number of objectives" << endl;
-    cin >> nbObj;
-
-    ifstream inFile("input/LAP_8x8_2_obj_SOL.txt");
-    inFile >> n;
-
-    int tab[32][n];
+    // Cost matrices
     for (int i = 0; i < 32; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < problem_size; j++)
         {
-            inFile >> tab[i][j];
-            cout << tab[i][j] << ' ';
+            inFile >> costs[i][j];
+            cout << costs[i][j] << ' ';
         }
         cout << endl;
     }
 
-    int nbSol;
-    inFile >> nbSol;
-    int sol[nbSol][nbObj];
-    for (int i = 0; i < nbSol; i++)
-    {
-        inFile >> sol[0][0];
-        for (int j = 0; j < nbObj; j++)
-        {
-            inFile >> sol[i][j];
+    int optimal_solution_number;
+    inFile >> optimal_solution_number;
 
-            cout << sol[i][j] << ' ';
+    // Optimal solution
+    for (int i = 0; i < optimal_solution_number; i++)
+    {
+        inFile >> opt_sol[0][0];
+        for (int j = 0; j < objectif_number; j++)
+        {
+            inFile >> opt_sol[i][j];
+            cout << opt_sol[i][j] << ' ';
         }
         cout << endl;
     }
-
-    // premiere solution
-
-    vector<int> A;
-    int p1[n];
-    int p2[n];
-    for (int i = 0; i < n; i++)
-    {
-        p1[i] = i;
-        p2[i] = i;
-    }
-
-    random_shuffle(p1, p1 + n);
-    random_shuffle(p2, p2 + n);
-
-    int X[n][2];
-    for (int i = 0; i < n; i++)
-    {
-        X[i][1] = p1[i];
-        X[i][2] = p2[i];
-    }
-
-    return 1;
 }
-
-// sol -> [(i,j), ()]
-
-// func evaluer entrer tab[i][j] et boucler sur tous les objectifs
-
-// recherche locale inverser les i ou les j
-
-// Algo ->
-// dominance si la solution est dominée ou pas dans l'archive
